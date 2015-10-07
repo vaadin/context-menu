3e26bd5ecdd9e1157994ef182a56ef7659230832
Sami Ekblad <sami@vaadin.com>
2015-10-08 00:22:16 +0300

    Project configuration update: use snapshot and Java 8. C orrect widgetset.

--- context-menu-demo/src/main/java/ContextmenuUI.java
+++ context-menu-demo/src/main/java/ContextmenuUI.java
@@ -2,23 +2,24 @@
 
 import com.vaadin.addon.contextmenu.ContextMenu;
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.Widgetset;
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import javax.servlet.annotation.WebServlet;
 
 @SuppressWarnings("serial")
 @Theme("contextmenu")
-@Widgetset("com.vaadin.addon.contextmenu.demo.DemoWidgetSet")
 public class ContextmenuUI extends UI {
 
-//	@WebServlet(value = "/*", asyncSupported = true)
-//	@VaadinServletConfiguration(productionMode = false, ui = ContextmenuUI.class, widgetset = "com.vaadin.addon.contextmenu.widgetset.ContextmenuWidgetset")
-//	public static class Servlet extends VaadinServlet {
-//	}
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = ContextmenuUI.class, widgetset = "com.vaadin.addon.contextmenu.demo.DemoWidgetSet")
+	public static class Servlet extends VaadinServlet {
+	}
 
 	@Override
 	protected void init(VaadinRequest request) {
