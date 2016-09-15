a838b6cc18c8cb503878738d1af5378e67f7ac34
Artur Signell <artur@vaadin.com>
2016-09-15 15:32:06 +0300

    Build snapshots from master using Travis

---
+++ .travis.after_success.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]
+then
+	cd vaadin-context-menu-addon
+	mvn deploy --settings ../.travis-settings.xml -DrepositoryId=vaadin-snapshots -DaltDeploymentRepository=vaadin-snapshots::default::https://oss.sonatype.org/content/repositories/vaadin-snapshots/ -DskipTests=true -DskipITs -B
+fi
+
