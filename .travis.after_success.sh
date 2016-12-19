504b7ab5e5cf92900a0b4c91463a94134b0e3fee
Artur <artur@vaadin.com>
2016-12-19 17:05:44 +0200

    Fix path to travis-settings.xml (#42)

--- .travis.after_success.sh
+++ .travis.after_success.sh
@@ -3,6 +3,6 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]
 then
 	cd vaadin-context-menu-addon
-	mvn deploy --settings .travis-settings.xml -DrepositoryId=vaadin-snapshots -DaltDeploymentRepository=vaadin-snapshots::default::https://oss.sonatype.org/content/repositories/vaadin-snapshots/ -DskipTests=true -DskipITs -B
+	mvn deploy --settings ../.travis-settings.xml -DrepositoryId=vaadin-snapshots -DaltDeploymentRepository=vaadin-snapshots::default::https://oss.sonatype.org/content/repositories/vaadin-snapshots/ -DskipTests=true -DskipITs -B
 fi
 
