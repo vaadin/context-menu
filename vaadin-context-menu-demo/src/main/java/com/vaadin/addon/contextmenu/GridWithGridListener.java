58392ceef012e9d7bfb80a4bdd6ee7a477bddcdd
Artur Signell <artur@vaadin.com>
2016-12-20 10:19:47 +0200

    Update to 8.0.0.beta1

--- vaadin-context-menu-demo/src/main/java/com/vaadin/addon/contextmenu/GridWithGridListener.java
+++ vaadin-context-menu-demo/src/main/java/com/vaadin/addon/contextmenu/GridWithGridListener.java
@@ -3,7 +3,7 @@ package com.vaadin.addon.contextmenu;
 import java.util.Collections;
 
 import com.vaadin.addon.contextmenu.GridContextMenu.GridContextMenuOpenListener.GridContextMenuOpenEvent;
-import com.vaadin.server.data.ListDataProvider;
+import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.ui.Grid;
 
 public class GridWithGridListener extends Grid<String[]> {
@@ -13,10 +13,8 @@ public class GridWithGridListener extends Grid<String[]> {
         setHeightByRows(3);
         GridContextMenu<String[]> gridContextMenu = new GridContextMenu<>(this);
 
-        addColumn("column 1", arr -> arr[0])
-                .setCaption("Column 1(right-click here)");
-        addColumn("column 2", arr -> arr[1])
-                .setCaption("Column 2(right-click here)");
+        addColumn(arr -> arr[0]).setCaption("Column 1(right-click here)");
+        addColumn(arr -> arr[1]).setCaption("Column 2(right-click here)");
 
         ListDataProvider<String[]> dataSource = new ListDataProvider<>(
                 Collections.singletonList(new String[] { "foo", "bar" }));
