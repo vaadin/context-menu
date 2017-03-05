ad8b3712aa77c6ee71a768bda3cadfaeec137b1f
Francois Steyn <tfyre@tfyre.co.za>
2017-03-05 14:19:37 +0200

    Fix #55: Implemented Generics

--- vaadin-context-menu-addon/src/main/java/com/vaadin/contextmenu/GridContextMenu.java
+++ vaadin-context-menu-addon/src/main/java/com/vaadin/contextmenu/GridContextMenu.java
@@ -19,19 +19,16 @@ public class GridContextMenu<T> extends ContextMenu {
 
     private void addGridSectionContextMenuListener(final Section section,
             final GridContextMenuOpenListener<T> listener) {
-        addContextMenuOpenListener(new ContextMenuOpenListener() {
-            @Override
-            public void onContextMenuOpen(ContextMenuOpenEvent event) {
-                if (event
-                        .getContextClickEvent() instanceof GridContextClickEvent) {
-                    @SuppressWarnings("unchecked")
-                    GridContextClickEvent<T> gridEvent = (GridContextClickEvent<T>) event
-                            .getContextClickEvent();
-                    if (gridEvent.getSection() == section) {
-                        listener.onContextMenuOpen(
-                                new GridContextMenuOpenEvent<T>(
-                                        GridContextMenu.this, gridEvent));
-                    }
+        addContextMenuOpenListener((final ContextMenuOpenEvent event) -> {
+            if (event
+                    .getContextClickEvent() instanceof GridContextClickEvent) {
+                @SuppressWarnings("unchecked")
+                GridContextClickEvent<T> gridEvent = (GridContextClickEvent<T>) event
+                        .getContextClickEvent();
+                if (gridEvent.getSection() == section) {
+                    listener.onContextMenuOpen(
+                            new GridContextMenuOpenEvent<>(
+                                    GridContextMenu.this, gridEvent));
                 }
             }
         });
@@ -59,29 +56,36 @@ public class GridContextMenu<T> extends ContextMenu {
 
         public static class GridContextMenuOpenEvent<T>
                 extends ContextMenuOpenEvent {
-            private final Object item;
+
+            private final T item;
+            private final Grid<T> component;
             private final int rowIndex;
-            private final Column<?, ?> column;
+            private final Column<T, ?> column;
             private final Section section;
 
             public GridContextMenuOpenEvent(ContextMenu contextMenu,
                     GridContextClickEvent<T> contextClickEvent) {
                 super(contextMenu, contextClickEvent);
                 item = contextClickEvent.getItem();
+                component = contextClickEvent.getComponent();
                 rowIndex = contextClickEvent.getRowIndex();
                 column = contextClickEvent.getColumn();
                 section = contextClickEvent.getSection();
             }
 
-            public Object getItem() {
+            public T getItem() {
                 return item;
+            }
+
+            public Grid<T> getComponent() {
+                return component;
             }
 
             public int getRowIndex() {
                 return rowIndex;
             }
 
-            public Column<?, ?> getColumn() {
+            public Column<T, ?> getColumn() {
                 return column;
             }
 
