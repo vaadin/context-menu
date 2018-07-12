10700eaad26d871482472ffeb12e27a785e52ed9
Elmot <elmot@vaadin.com>
2018-07-12 11:28:01 +0300

    Add license headers

--- vaadin-context-menu-addon/src/main/java/com/vaadin/contextmenu/client/ContextMenuConnector.java
+++ vaadin-context-menu-addon/src/main/java/com/vaadin/contextmenu/client/ContextMenuConnector.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.vaadin.contextmenu.client;
 
 import com.google.gwt.core.client.GWT;
@@ -191,19 +207,6 @@ public class ContextMenuConnector extends AbstractExtensionConnector
                 rootSubMenu.setSelected(items.get(0));
             }
         });
-
-        /*
-         * Event.addNativePreviewHandler(new Event.NativePreviewHandler() {
-         *
-         * @Override public void onPreviewNativeEvent(Event.NativePreviewEvent
-         * event) { if (event.getTypeInt() == Event.ONKEYDOWN &&
-         * contextMenu.isVisible()) { boolean handled =
-         * contextMenu.handleNavigation( event.getNativeEvent().getKeyCode(),
-         * event.getNativeEvent().getCtrlKey(),
-         * event.getNativeEvent().getShiftKey());
-         *
-         * if (handled) { event.cancel(); } } } });
-         */
 
     }
 
