ac81904f98cb7d2193859123108083994546f528
thevaadinman <patrik@vaadin.com>
2025-07-17 12:16:11 +0300

    chore: Update project structure and tooling for Vaadin 7.7.17

--- vaadin-context-menu-addon/src/test/java/com/vaadin/addon/contextmenu/ContextMenuTest.java
+++ vaadin-context-menu-addon/src/test/java/com/vaadin/addon/contextmenu/ContextMenuTest.java
@@ -1,11 +1,11 @@
 package com.vaadin.addon.contextmenu;
 
-import junit.framework.Assert;
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 // JUnit tests here
 public class ContextMenuTest {
-
     @Test
     public void thisAlwaysPasses() {
         Assert.assertEquals(true, true);
